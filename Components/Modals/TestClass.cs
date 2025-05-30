fe54c80634942ee5382f085367383f883988576e
JIWON <JIWON@JIWON-DESKTOP01>
2025-05-30 16:14:42 +0900

    111

---
+++ Components/Modals/TestClass.cs
@@ -0,0 +1,23 @@
+﻿namespace StockProject.Components.Modals
+{
+    public class TestClass : IEquatable<TestClass?>
+    {
+        public int MyProperty { get; set; }
+
+        public override bool Equals(object? obj)
+        {
+            return Equals(obj as TestClass);
+        }
+
+        public bool Equals(TestClass? other)
+        {
+            return other is not null &&
+                   MyProperty == other.MyProperty;
+        }
+
+        public override int GetHashCode()
+        {
+            return HashCode.Combine(MyProperty);
+        }
+    }
+}
