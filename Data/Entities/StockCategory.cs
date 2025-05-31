eabcd3394313930f1c39eed2f4218b080bce681e
WJEIROWERJOI <kgwball@hotmail.com>
2025-05-31 22:43:47 +0900

    qqqq

--- Data/Entities/StockCategory.cs
+++ Data/Entities/StockCategory.cs
@@ -1,11 +1,13 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
 namespace StockProject.Data.Entities;
+
 public class StockCategory
 {
     [Key]
     public int CategoryId { get; set; }
     public string Name { get; set; } = string.Empty;
     public string Description { get; set; } = string.Empty;
+    public List<StockEntity> Stocks { get; set; } = new();
 }
 
