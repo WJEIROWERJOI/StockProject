b1db173eeb5be26ddfe1f4112f23aa3c7dc9af29
JIWON <JIWON@JIWON-DESKTOP01>
2025-06-06 01:32:36 +0900

    aaaaa

--- Data/Entities/Point.cs
+++ Data/Entities/Point.cs
@@ -4,9 +4,8 @@ public class Point
 {
     public int Id { get; set; }
     public string UserId { get; set; } = null!;
-    public int Amount { get; set; }
+    public int Money { get; set; }
     public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
     public DateTime UpdatedAt { get; set; } = DateTime.UtcNow;
-
     public UserEntity User { get; set; } = null!;
 }
---
+++ Data/Services/PointService.cs
@@ -0,0 +1,13 @@
+﻿namespace StockProject.Data.Services
+{
+    public class PointService
+    {
+        private readonly ApplicationDbContext _context;
+        public PointService(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+
+    }
+}
